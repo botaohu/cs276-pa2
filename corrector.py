1049a15cca7f96772875ab5089bdac6370dd8e18
Botao Hu <me@botao.hu>
2013-05-02 23:33:00 -0700

    sdf

--- corrector.py
+++ corrector.py
@@ -1,5 +1,9 @@
-
 import sys
+import marshal
+import itertools
+import operator
+import math
+from collections import deque
 
 queries_loc = 'data/queries.txt'
 gold_loc = 'data/gold.txt'
@@ -7,10 +11,116 @@ google_loc = 'data/google.txt'
 
 alphabet = "abcdefghijklmnopqrstuvwxyz0123546789&$+_' "
 
+def unserialize_data(fname):
+  """
+  Reads a pickled data structure from a file named `fname` and returns it
+  IMPORTANT: Only call marshal.load( .. ) on a file that was written to using marshal.dump( .. )
+  marshal has a whole bunch of brittle caveats you can take a look at in teh documentation
+  It is faster than everything else by several orders of magnitude though
+  """
+  with open(fname, 'rb') as f:
+    return marshal.load(f)
+
+def candidate_gen(word, index, word_dict, thresh):
+  """
+  Generate candidate using k-gram index
+  """
+  word_aug = '$' + word + '$'
+  idx = range(len(word_aug))
+  query = set()
+  for tup in itertools.izip(idx[:-1],idx[1:]):
+    query.add(word_aug[tup[0]]+word_aug[tup[1]])
+  candidate = {}
+  query = [u for u in query if u in index]
+  for item in query:
+    lst = [word_dict[u] for u in index[item] if abs(len(word_dict[u])-len(word)) <= 2]
+    for w in lst:
+      if w in candidate:
+        candidate[w] += 1
+      else:
+        candidate[w] = 1
+  cand = []
+  for key in candidate:
+    score = float(candidate[key]) / float(len(key) + 1 + len(query) - candidate[key])
+    if score >= thresh:
+      cand.append(key)
+  return cand
+
+def edit_distance(word1, word2):
+  """
+  Compute exact edit distance
+  """
+  w1 = ' ' + word1
+  w2 = ' ' + word2
+  d = {}
+  for i in range(len(w1)):
+    d[i,0] = i
+  for j in range(len(w2)):
+    d[0,j] = j
+  for i in range(1, len(w1)):
+    for j in range(1, len(w2)):
+      if w1[i] == w2[j]:
+        d[i,j] = d[i-1,j-1]
+      else:
+        d[i,j] = min(d[i-1,j] + 1, d[i,j-1] + 1, d[i-1,j-1] + 1)
+      if i > 1 and j > 1 and w1[i] == w2[j-1] and w1[i-1] == w2[j]:
+        d[i,j] = min(d[i,j], d[i-2,j-2] + 1)
+  return d[len(w1)-1, len(w2)-1]
+  
+def language_model(query, prob):
+  """
+  Compute the score of language model
+  """
+  score = 0.0
+  score += math.log10(prob[query[0]])
+  idx = range(len(query))
+  for tup in itertools.izip(idx[:-1],idx[1:]):
+    bigram = (query[tup[0]], query[tup[1]])
+    score += math.log10(prob[bigram])
+  return score
+
+
+def noisy_model(correct, wrong, model):
+  """
+  Noisy edit model
+  """
+  if correct == wrong:
+    return math.log10(0.90)
+  else:
+    return math.log10(0.05)
+
+def gen_candidate_query(query, index, word_dict, prob, thresh):
+  """
+  Generate possible query
+  """
+  queue = deque([])
+  lst = candidate_gen(query[0], index, word_dict, thresh)
+  lst = [u for u in lst if edit_distance(u, query[0]) <= 2]
+  for item in lst:
+    queue.append([item])
+  for i in range(1, len(query)):
+    lst = candidate_gen(query[i], index, word_dict, thresh)
+    lst = [u for u in lst if edit_distance(u, query[i]) <= 2]
+    while len(queue) != 0:
+      seq = queue.popleft()
+      if len(seq) == i+1:
+        queue.append(seq)
+        break
+      for item in lst:
+        if (seq[-1], item) in prob:
+          queue.append(seq + [item])
+  return queue
+
+def score(original, current, lang_model, edit_model):
+  sc = language_model(current, lang_model)
+  for i in range(len(original)):
+    sc += noisy_model(original[i], current[i], edit_model)
+  return sc
+
+
 def read_query_data():
   """
   all three files match with corresponding queries on each line
-  NOTE: modify the signature of this method to match your program's needs
   """
   queries = []
   gold = []
@@ -28,4 +138,35 @@ def read_query_data():
   return (queries, gold, google)
 
 if __name__ == '__main__':
-  print(sys.argv)
+  lang = unserialize_data("language_model")
+  noisy = unserialize_data("edit_model")
+  index = unserialize_data("index")
+  word_dict = unserialize_data("word")
+  data = read_query_data()
+
+  # Parameters
+  thresh = 0.5
+
+  question = data[0]
+  answer = data[1]
+  google = data[2]
+
+  acc = len(answer)
+  print acc
+  for i in range(len(question)):
+    if i % 100 == 0:
+      print "Progess %d" % i
+    qry = question[i].split()
+    cand = gen_candidate_query(qry, index, word_dict, lang, thresh)
+    ans = ''
+    if len(cand) == 0:
+      ans = ' '.join(qry)
+    else:
+      rank = [score(qry, u, lang, noisy) for u in cand]
+      idx = rank.index(max(rank))
+      ans = ' '.join(cand[idx])
+    if ans != answer[i]:
+      acc -= 1
+  print 'Accuracy: %d' % acc
+    
+    
--- models.py
+++ models.py
@@ -1,33 +1,157 @@
-
 import sys
 import os.path
-#import gzip
+import itertools
+import math
+import marshal
 from glob import iglob
 
-def scan_corpus(training_corpus_loc):
+def serialize_data(data, fname):
   """
-  Scans through the training corpus and counts how many lines of text there are
+  Writes `data` to a file named `fname`
   """
-  for block_fname in iglob( os.path.join( training_corpus_loc, '*.txt' ) ):
-    print >> sys.stderr, 'processing dir: ' + block_fname
-    with open( block_fname ) as f:
-      num_lines = 0
-      for line in f:
-        # remember to remove the trailing \n
-        line = line.rstrip()
-        num_lines += 1
-      print >> sys.stderr, 'Number of lines in ' + block_fname + ' is ' + str(num_lines)
+  with open(fname, 'wb') as f:
+    marshal.dump(data, f)
 
-def read_edit1s():
+def add_table(dic, key):
+  if key not in dic:
+    dic[key] = 1
+  else:
+    dic[key] += 1
+
+def load_corpus(training_corpus_loc):
   """
-  Returns the edit1s data
-  It's a list of tuples, structured as [ .. , (misspelled query, correct query), .. ]
+  Scans through the training corpus
+    1. counts how many bigrams and unigrams there are
+    2. builds bigram indices
   """
-  edit1s = []
-  with open(edit1s_loc) as f:
-    # the .rstrip() is needed to remove the \n that is stupidly included in the line
-    edit1s = [ line.rstrip().split('\t') for line in f if line.rstrip() ]
-  return edit1s
+  count, bigram_idx, word_dict = {}, {}, {}
+  tot_cnt, word_id = 0, 1
+  for block_fname in iglob(os.path.join( training_corpus_loc, '*.txt')):
+    print >> sys.stderr, 'processing dir: ' + block_fname
+    doc = []
+    with open(block_fname, 'r') as f:
+      for line in f.readlines():
+        line = line.strip().split()
+        doc += line
+    tot_cnt += len(doc)
+    for unigram in doc:
+      # Bigram index
+      if unigram not in count:
+        word_dict[word_id] = unigram
+        temp = '$' + unigram + '$'
+        i = range(len(temp))
+        for tup in itertools.izip(i[:-1],i[1:]):
+          k = temp[tup[0]] + temp[tup[1]]
+          if k not in bigram_idx:
+            bigram_idx[k] = [word_id]
+          else:
+            bigram_idx[k].append(word_id)
+        word_id += 1
+      add_table(count, unigram)
+      
+    idx = range(len(doc))
+    for tup in itertools.izip(idx[:-1],idx[1:]):
+      bigram = (doc[tup[0]], doc[tup[1]])
+      add_table(count, bigram)
+      
+  # Sorting bigram index
+  for k in bigram_idx:
+    bigram_idx[k] = sorted(set(bigram_idx[k]))
+  return count, tot_cnt, bigram_idx, word_dict
+
+def calc_prob(count, tot_cnt, lam):
+  """
+  Calculating probabilities in log-space using unigram/bigram count
+  """
+  prob = {}
+  for item in count:
+    if isinstance(item, tuple):
+      prob[item] = float(count[item]) / float(count[item[0]])
+    elif isinstance(item, str):
+      prob[item] = float(count[item]) / float(tot_cnt)
+  for item in prob:
+    if isinstance(item, tuple):
+      prob[item] = (1-lam) * prob[item] + lam * prob[item[1]]
+  return prob
+
+def edit_type(wrong, correct):
+  """
+  Deciding which type of edits occured in this pair of string
+  """
+  if len(correct) == len(wrong):
+    for i in range(0, len(correct)):
+      if correct[i] != wrong[i]:
+        # Transposition
+        if i != len(correct) - 1 and correct[i] == wrong[i+1]:
+          return (correct[i]+correct[i+1], wrong[i]+wrong[i+1])
+        # Substitution
+        else:
+          return (correct[i], wrong[i])
+  elif len(correct) > len(wrong):
+    # Deletion
+    for i in range(0, len(wrong)):
+      if wrong[i] != correct[i]:
+        return (correct[i]+correct[i+1], wrong[i])
+    return (correct[-2:], wrong[-1])
+  else:
+    # Insertion
+    for i in range(0, len(correct)):
+      if correct[i] != wrong[i]:
+        return (correct[i], wrong[i]+wrong[i+1])
+    return (correct[-1], wrong[-2:])
+  assert(1 == 0)
+  
+def noisy_model(edit_file):
+  """
+  Returns the counts
+  """
+  noisy_model = {}
+  with open(edit_file, 'r') as f:
+    for line in f.readlines():
+      line = line.rstrip().split('\t')
+      # Type error
+      if line[0] != line[1]:
+        tup = edit_type(line[0], line[1])
+        add_table(noisy_model, tup)
+        
+      # Singleton
+      for t in line[1]:
+        add_table(noisy_model, t)
+
+      # Bigram
+      idx = range(len(line[1]))
+      for tup in itertools.izip(idx[:-1],idx[1:]):
+        add_table(noisy_model, line[1][tup[0]]+line[1][tup[1]])
+  return noisy_model
+
 
 if __name__ == '__main__':
-  print(sys.argv)
+  if len(sys.argv) != 3 and len(sys.argv) != 4:
+    print >> sys.stderr, 'usage: python models.py [extra] <training corpus dir> <training edit1s file>'
+    os._exit(-1)
+  elif len(sys.argv) == 4:
+    assert(sys.argv[1] == "extra")
+    train_lang_dir = sys.argv[2]
+    train_edit_dir = sys.argv[3]
+  else:
+    train_lang_dir = sys.argv[1]
+    train_edit_dir = sys.argv[2]
+
+  inter_lambda = 0.2
+  count, tot, index, word_dict = load_corpus(train_lang_dir)
+  
+  prob = calc_prob(count, tot, inter_lambda)
+  serialize_data(prob, "language_model")
+  serialize_data(index, "index")
+  serialize_data(word_dict, "word")
+
+  edit = noisy_model(train_edit_dir)
+  serialize_data(edit, "edit_model")
+
+  
+  
+
+  
+  
+    
+  
